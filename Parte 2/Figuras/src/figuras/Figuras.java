3d3eae8aa1d84000492c1216d77533fbd58655e1
Jessica Vega <jevegaa@unal.edu.co>
2022-10-03 00:13:03 -0500

    Create Figuras.java

---
+++ Parte 2/Figuras/src/figuras/Figuras.java
@@ -0,0 +1,12 @@
+package figuras;
+/**
+ *
+ * @author Jessica Vega
+ */
+public class Figuras {
+        public static void main(String[] args) {
+        Ventana myventana = new Ventana(); //método
+            myventana.show(true);
+    } 
+
+}
