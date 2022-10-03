423f5667c4fb454f3191b83586ace14a4d920a78
Jessica Vega <jevegaa@unal.edu.co>
2022-10-03 00:03:53 -0500

    Create Empleado.java

---
+++ Parte 1/E22/src/e22/Empleado.java
@@ -0,0 +1,13 @@
+package e22;
+/**
+ *
+ * @author Jessica Vega
+ */
+public class Empleado{ 
+    public static double Salario(double salarioHora, double horas){
+        double salarioMes;
+        
+        salarioMes=salarioHora*horas;
+        return salarioMes;   
+    }
+}
