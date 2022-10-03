9ff5763aa93986aaa6cbeeb28915d3aae88343e6
Jessica Vega <jevegaa@unal.edu.co>
2022-10-03 00:06:06 -0500

    Create E23.java

---
+++ Parte 1/E23/src/e23/E23.java
@@ -0,0 +1,12 @@
+package e23;
+/**
+ *
+ * @author Jessica Vega
+ */
+public class E23 {
+    public static void main(String[] args) {
+        Ventana6 myventana6 = new Ventana6(); //método
+            myventana6.show(true);
+    }
+    
+}
