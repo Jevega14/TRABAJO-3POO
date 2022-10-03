dd998e17e46452d1797633a43e94bc8acbb376d9
Jessica Vega <jevegaa@unal.edu.co>
2022-10-02 23:55:22 -0500

    Create E10.java

---
+++ Parte 1/E10/src/e10/E10.java
@@ -0,0 +1,11 @@
+package e10;
+/**
+ *
+ * @author Jessica Vega
+ */
+public class E10 {
+    public static void main(String[] args) {
+        Ventana4 myventana4 = new Ventana4(); //método
+            myventana4.show(true);
+    }   
+}
