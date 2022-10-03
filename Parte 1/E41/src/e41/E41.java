99b7f657f578d8006878f89c55a8f17e341b467d
Jessica Vega <jevegaa@unal.edu.co>
2022-10-03 00:09:08 -0500

    Create E41.java

---
+++ Parte 1/E41/src/e41/E41.java
@@ -0,0 +1,14 @@
+package e41;
+/**
+ *
+ * @author Jessica Vega
+ */
+public class E41 {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Ventana8 myventana8 = new Ventana8(); //método
+            myventana8.show(true);
+    } 
+}
