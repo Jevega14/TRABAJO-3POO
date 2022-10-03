be4e01e84d57a88fe6ce3d20161a9435a431556a
Jessica Vega <jevegaa@unal.edu.co>
2022-10-03 00:00:33 -0500

    Create E19.java

---
+++ Parte 1/E19/src/e19/E19.java
@@ -0,0 +1,12 @@
+package e19;
+/**
+ *
+ * @author Jessica Vega
+ */
+public class E19 {
+
+    public static void main(String[] args) {
+        Ventana2 myventana2 = new Ventana2(); //método
+            myventana2.show(true);
+    }          
+}
