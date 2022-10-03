4cf0c11cd8a6e2a6f8c1e516c1ab1f639efd9efe
Jessica Vega <jevegaa@unal.edu.co>
2022-10-02 23:57:47 -0500

    Create Trabajo3.java

---
+++ Parte 1/E18/src/E18/Trabajo3.java
@@ -0,0 +1,11 @@
+
+package E18;
+public class Trabajo3 {
+
+    public static void main(String[] args) {
+        Ventana myventana = new Ventana(); //método
+            myventana.show(true);
+    }
+
+
+}
