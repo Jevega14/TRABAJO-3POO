96d2bd142531d5b2b9ddbaab0a82fba79bfc955e
Jessica Vega <jevegaa@unal.edu.co>
2022-10-03 00:11:05 -0500

    Create E7.java

---
+++ Parte 1/E7/src/e7/E7.java
@@ -0,0 +1,13 @@
+package e7;
+/**
+ *
+ * @author Jessica Vega
+ */
+public class E7 {
+    
+    public static void main(String[] args) {
+        Ventana3 myventana3 = new Ventana3(); //método
+            myventana3.show(true);
+    }        
+    
+}
