be060bf895521cf605ad88bbeac36213387c36ff
Jessica Vega <jevegaa@unal.edu.co>
2022-10-03 00:11:01 -0500

    Create Comparar.java

---
+++ Parte 1/E7/src/e7/Comparar.java
@@ -0,0 +1,22 @@
+package e7;
+/**
+ *
+ * @author Jessica Vega
+ */
+public class Comparar {
+    
+    public static String MayorMenor (double A, double B){
+        
+        if (A>B){
+            return "A es mayor que B";
+        }
+        else if (A<B){
+            return "A es menor que B";
+        }
+        else{
+            return "A es igual que B";
+        }
+      
+    }
+    
+}
