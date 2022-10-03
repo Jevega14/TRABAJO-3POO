633ae24cebb0016eaaed360b72e4553b671ca965
Jessica Vega <jevegaa@unal.edu.co>
2022-10-03 00:12:56 -0500

    Create Cuadrado.java

---
+++ Parte 2/Figuras/src/figuras/Cuadrado.java
@@ -0,0 +1,15 @@
+package figuras;
+/**
+ *
+ * @author Jessica Vega
+ */
+public class Cuadrado {
+    
+    public static double calcularArea(double lado) {
+        return lado*lado;
+        }
+
+    public static double calcularPerímetro(double lado) {
+        return (4*lado);
+        }
+}
