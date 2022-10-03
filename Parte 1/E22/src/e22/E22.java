eb0552845bf5f20d9ad00b6ee46d84c46ca594bf
Jessica Vega <jevegaa@unal.edu.co>
2022-10-03 00:03:49 -0500

    Create E22.java

---
+++ Parte 1/E22/src/e22/E22.java
@@ -0,0 +1,12 @@
+package e22;
+/**
+ *
+ * @author Jessica Vega
+ */
+public class E22 {
+    public static void main(String[] args) {
+        Ventana5 myventana5 = new Ventana5(); //método
+            myventana5.show(true);
+    }
+    
+}
