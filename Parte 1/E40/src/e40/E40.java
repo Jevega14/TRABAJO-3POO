056719fe5ab80b2f34b27b023ef27ac3ad5775a5
Jessica Vega <jevegaa@unal.edu.co>
2022-10-03 00:07:39 -0500

    Create E40.java

---
+++ Parte 1/E40/src/e40/E40.java
@@ -0,0 +1,12 @@
+package e40;
+/**
+ *
+ * @author Jessica Vega
+ */
+public class E40 {
+    public static void main(String[] args) {
+        Ventana7 myventana7 = new Ventana7(); //método
+            myventana7.show(true);
+    }
+    
+}
